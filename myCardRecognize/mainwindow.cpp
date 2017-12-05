ba1f1c92f79db7a6bc219ed7dbb47b5332f9ba75
OldFire <hongyang.ruan@outlook.com>
2017-12-05 01:07:09 -0800

    20171205接入阿里云机器人

--- myCardRecognize/mainwindow.cpp
+++ myCardRecognize/mainwindow.cpp
@@ -11,6 +11,14 @@ MainWindow::MainWindow(QWidget *parent) :
        QObject::connect(nam, SIGNAL(finished(QNetworkReply*)),
                 this, SLOT(finishedSlot(QNetworkReply*)));
 
+    m_accessManager=new QNetworkAccessManager(this);
+    QObject::connect(m_accessManager, SIGNAL(finished(QNetworkReply*)),
+             this, SLOT(finishedGet(QNetworkReply*)));
+
+    ui->edt_send->setText("hello");
+     connect(ui->edt_talk,SIGNAL(textChanged()),this,SLOT(movetoEnd()));
+
+     connect(ui->edt_send, SIGNAL(returnPressed()), this, SLOT(on_btn_send_clicked()));
 
 }
 
@@ -18,6 +26,17 @@ MainWindow::~MainWindow()
 {
     delete ui;
     delete nam;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    if(ui->edt_send->text().isEmpty()||ui->edt_send->text().isNull()||ui->edt_send->text()=="") return;
+
+    if(event->key() == Qt::Key_Return)
+    {
+        this->on_btn_send_clicked();
+    }
+
 }
 
 QString MainWindow::getIdCardNumber(QString &str)
@@ -108,7 +127,49 @@ void MainWindow::finishedSlot(QNetworkReply *reply)
              qDebug(qPrintable(reply->errorString()));
              ui->edt_ResData ->setText("sorry,don't recoginzed your card number!");
          }
-         reply->deleteLater();
+    reply->deleteLater();
+}
+
+void MainWindow::finishedGet(QNetworkReply *reply)
+{
+
+    if (reply->error() == QNetworkReply::NoError)
+    {
+        QByteArray bytes = reply->readAll();
+        QJsonDocument doc=QJsonDocument::fromJson(bytes);
+        if(doc.isObject())
+        {
+            QJsonObject obj= doc.object();
+            if(obj.contains("result"))
+            {
+                QJsonValue val=obj.take("result");
+                if(val.isObject())
+                {
+                    QJsonObject obj1= val.toObject();
+                    if(obj1.contains("content"))
+                    {
+                        QString resdata=obj1.take("content").toString();
+                        all_msg+="recv:\n  "+QString(resdata)+"\n";
+                        ui->edt_talk->setText(all_msg);
+                    }
+                }
+            }
+        }
+
+    }
+    else
+    {
+        qDebug()<<"handle errors here";
+        QVariant statusCodeV = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+        //statusCodeV是HTTP服务器的相应码，reply->error()是Qt定义的错误码，可以参考QT的文档
+        qDebug( "found error ....code: %d %d\n", statusCodeV.toInt(), (int)reply->error());
+        qDebug(qPrintable(reply->errorString()));
+        all_msg+="recv:\n  "+QString("sorry,i don't konw what are you say...")+"\n";
+        ui->edt_talk ->setText(all_msg);
+
+    }
+    reply->deleteLater();
+
 }
 
 
@@ -121,6 +182,11 @@ void MainWindow::replyErrprSlot()
 
 void MainWindow::on_btn_PostReq_clicked()
 {
+    if(FileName.isEmpty()||FileName==" ")
+        {
+            QMessageBox::warning(NULL,"warning","文件类型错误或未选择文件，请重新输入！",QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+            return;
+        }
     QImage image(FileName);
         QByteArray ba;
         QBuffer buf(&ba);
@@ -156,6 +222,8 @@ void MainWindow::on_btn_PostReq_clicked()
         QNetworkReply* reply = nam->post(*request,mybody );
         connect(reply,SIGNAL(error(QNetworkReply::NetworkError)),this,SLOT(replyErrprSlot()));
 
+
+        delete request;
 }
 
 void MainWindow::on_btn_GetReq_clicked()
@@ -169,4 +237,45 @@ void MainWindow::on_btn_selectFile_clicked()
     FileName =QFileDialog::getOpenFileName(NULL,"Open Image", "./", "Image Files (*.png *.jpg *.bmp)");
     ui->edt_filePath->setText(FileName);
     ui->lab_image->setPixmap(QPixmap(FileName));
+}
+
+void MainWindow::on_btn_refresh_clicked()
+{
+    all_msg="";
+    ui->edt_talk->clear();
+
+}
+
+void MainWindow::on_btn_send_clicked()
+{
+
+    QString msg=ui->edt_send->text();
+
+    if(msg.isEmpty()||msg=="")
+        {
+            QMessageBox::warning(NULL,"warning","消息为空，请重新输入！",QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+            return;
+        }
+    QByteArray addr("http://jisuznwd.market.alicloudapi.com/iqa/query?question=");
+
+    QUrl myurl(addr+msg.toUtf8());
+   // qDebug()<<addr+msg.toUtf8();
+    QNetworkRequest request;
+   request.setUrl(myurl);
+   request.setRawHeader("Content-Type","application/json; charset=utf-8");
+   request.setRawHeader("Accept","application/json");
+   request.setRawHeader("Authorization","APPCODE a13ff1330a354694ae17ab6defe32a78");
+   m_accessManager->get(request);
+
+
+   all_msg+="send:\n  "+msg+"\n";
+   ui->edt_send->clear();
+}
+
+void MainWindow::movetoEnd()
+{
+    QTextCursor cursor=ui->edt_talk->textCursor();
+    cursor.movePosition(QTextCursor::End);
+    ui->edt_talk->setTextCursor(cursor);
+
 }
--- myCardRecognize/mainwindow.h
+++ myCardRecognize/mainwindow.h
@@ -12,6 +12,9 @@
 #include <QJsonDocument>
 #include <QVariant>
 #include <QJsonArray>
+#include <QByteArray>
+#include <QMessageBox>
+#include <QKeyEvent>
 
 namespace Ui {
 class MainWindow;
@@ -24,6 +27,8 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+ protected:
+    void keyPressEvent(QKeyEvent * event);
 
 private slots:
     void on_btn_PostReq_clicked();
@@ -33,15 +38,28 @@ private slots:
     void on_btn_selectFile_clicked();
 
     void finishedSlot(QNetworkReply *reply);
+
+    void finishedGet(QNetworkReply *reply);
+
     void replyErrprSlot();
 
+    void movetoEnd();
+
     QString getIdCardNumber(QString &data);
+
+    void on_btn_refresh_clicked();
+
+    void on_btn_send_clicked();
 
 private:
     Ui::MainWindow *ui;
     QString FileName;
     QNetworkAccessManager *nam;
     QNetworkRequest *request;
+
+    QNetworkAccessManager *m_accessManager;
+    QString all_msg;
+
 };
 
 #endif // MAINWINDOW_H
